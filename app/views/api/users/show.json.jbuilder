396d9908f29a3854108d61e7463aa73889aa6a88
Borrom <borrom7@gmail.com>
2015-10-12 19:16:21 +0700

    Add users

---
+++ app/views/api/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @api_user, :id, :name, :password, :email, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    root 'users#index'
+    resources :users
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
